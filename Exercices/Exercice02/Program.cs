56359f0cdb7b9a45a72d4b3298a4912d69344446
Enzo1105 <94967159+Enzo1105@users.noreply.github.com>
2021-11-24 12:51:55 +0100

    Add files via upload

---
+++ Exercices/Exercice02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b, c;
+            Console.Write("Valeur de a : ");
+            int a = int.Parse(Console.ReadLine());
+
+
+
+            while(true)
+            {
+                Console.Write("Valeur de b : ");
+                b = int.Parse(Console.ReadLine());
+                if (b > a) break;
+                Console.WriteLine($"\t Vous devez saisir une valeur supérieur à {a}");
+            }
+
+            while(true)
+            {
+                Console.Write("Valeur de c : ");
+                c = int.Parse(Console.ReadLine());
+                if (c > b) break;
+                Console.WriteLine($"\t Vous devez saisir une valeur supérieur à {b}");
+            }
+            Console.WriteLine("Saisie correcte");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercices/Exercice03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercice03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string separateur = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+            while(true) // while = boucle 
+            {
+                Console.Write("Note : ");
+                string valeurSaisie = Console.ReadLine();
+                if (valeurSaisie.ToLower() == "ab") break;
+                // contrôle du séparateur
+                if (separateur == ",")
+                {
+                    valeurSaisie = valeurSaisie.Replace('.', '.');
+                }
+                else
+                {
+                    valeurSaisie = valeurSaisie.Replace('.', '.');
+                }
+                if (decimal.TryParse(valeurSaisie, out decimal note))
+                {           
+                    if ( note < 0 || note > 20 || note * 2 != Math.Truncate(note * 2)) // les ||veulent dire OU
+                    {
+                        Console.WriteLine("\t Note invalide");
+                    }
+                    else
+                    {
+                        break; // break = sortir de la boucle
+                    }
+                }
+            }
+
+            Console.WriteLine("Note correcte");
+            Console.ReadKey();
+        }
+    }
+}
